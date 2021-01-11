6cef3ac99ebe0e3ffaa9a6de1b1ed051da4d2987
Michael Merrill <michael.merrill@vonage.com>
2021-01-11 17:46:30 -0500

    Update build.sh

--- irsa/runner/build.sh
+++ irsa/runner/build.sh
@@ -1,7 +1,7 @@
 mvn clean package
 export AWS_REGION="us-east-1"
-export AWS_ACCOUNT="565009088587"
-export AWS_ROLE_ARN="arn:aws:iam::565009088587:role/k8s-ecr"
+export AWS_ACCOUNT="<AWS ACCOUNT>"
+export AWS_ROLE_ARN="arn:aws:iam::<AWS ACCOUNT>:role/k8s-ecr"
 export AWS_WEB_IDENTITY_TOKEN_FILE="token.txt" 
 java -jar target/tester-1.0.0.jar 
 
